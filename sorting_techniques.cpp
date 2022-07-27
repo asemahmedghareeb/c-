a00a292a9c82aa5970488682dd8b46d806122d25
asemahmedghareeb <asemelbadahy@gmail.com>
2022-07-27 18:31:29 +0200

    fix eror

---
+++ sorting_techniques.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+void code(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+int main(){
+code();
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
